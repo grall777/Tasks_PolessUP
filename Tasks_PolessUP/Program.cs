78e74ac32a4caceb6e910bc526d9aa12f67ea619
Grall <vlad_shibut@mail.ru>
2023-04-02 15:32:45 +0300

    Добавьте файлы проекта.

---
+++ Tasks_PolessUP/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks_PolessUP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tasks task = new Tasks();
+            task.Task_1();
+
+            int[] nums = new int[4] {8, 5, 1, 15};
+            int target = 9;
+            int[] index = task.Task_2(nums, target);
+            Console.WriteLine("[" + index[0] + "," + index[1] + "]");
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tasks_PolessUP/Tasks.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks_PolessUP
+{
+    public class Tasks
+    {
+        public void Task_1()
+        {
+            Console.WriteLine("Task 1: \n");
+            Console.WriteLine(" +\"\"\"\"\"+");
+            Console.WriteLine("(| o o |)");
+            Console.WriteLine(" |  ^  |");
+            Console.WriteLine(" | '-' |");
+            Console.WriteLine(" +-----+");
+        }
+        public int[] Task_2(int[] arrayNums, int target)
+        {
+            Console.WriteLine("\nTask 2: \n");
+            int[] index = new int[2];
+            for (int i = 0; i < arrayNums.Length; i++)
+            {
+                for (int j = i + 1; j < arrayNums.Length - 1; j++)
+                {
+                    if (arrayNums[i] + arrayNums[j] == target)
+                    {
+                        index[0] = i;
+                        index[1] = j;
+                        return index;
+                    }
+                }
+            }
+            Console.WriteLine("no value");
+            return index;
+
+        }
+    }
+}
